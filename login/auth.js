94a01337c6bc9b6049a30d95120af88d53480ffa
abdo12249 <abdobwd87@gmail.com>
2025-10-22 16:29:25 +0300

    Update Firebase authDomain in auth.js

--- login/auth.js
+++ login/auth.js
@@ -13,7 +13,7 @@ import {
 // Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyAQpXUUOLyN2B6IWGb5Ru2Dl8NZPNimTEg",
-  authDomain: "myanima.ddns.net",
+  authDomain: "wep1-25124.firebaseapp.com",
   projectId: "wep1-25124",
   storageBucket: "wep1-25124.firebasestorage.app",
   messagingSenderId: "400763524699",

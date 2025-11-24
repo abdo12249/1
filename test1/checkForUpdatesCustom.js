25ba6492aa5105bdca02f1cef844fd1c886fca52
abdo12249 <abdobwd87@gmail.com>
2025-11-24 20:18:57 +0200

    تحديث checkForUpdatesCustom.js بإضافة ao-no-orchestra-season-2.json

--- test1/checkForUpdatesCustom.js
+++ test1/checkForUpdatesCustom.js
@@ -1,1 +1,1 @@
-checkForUpdatesCustom(["https://myanima.ddns.net/test1/episodes/mofa-gongzhu-de-xiao-fannao.json", "https://myanima.ddns.net/test1/episodes/egao-no-taenai-shokuba-desu.json"]);
+checkForUpdatesCustom(["https://myanima.ddns.net/test1/episodes/mofa-gongzhu-de-xiao-fannao.json", "https://myanima.ddns.net/test1/episodes/egao-no-taenai-shokuba-desu.json", "https://myanima.ddns.net/test1/episodes/ao-no-orchestra-season-2.json"]);

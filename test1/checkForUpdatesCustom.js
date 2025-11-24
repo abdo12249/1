b24963f71d9fbea258efbbddcfd4a0fc83282946
abdo12249 <abdobwd87@gmail.com>
2025-11-24 20:18:51 +0200

    تحديث checkForUpdatesCustom.js بإضافة egao-no-taenai-shokuba-desu.json

--- test1/checkForUpdatesCustom.js
+++ test1/checkForUpdatesCustom.js
@@ -1,1 +1,1 @@
-checkForUpdatesCustom(["https://myanima.ddns.net/test1/episodes/mofa-gongzhu-de-xiao-fannao.json"]);
+checkForUpdatesCustom(["https://myanima.ddns.net/test1/episodes/mofa-gongzhu-de-xiao-fannao.json", "https://myanima.ddns.net/test1/episodes/egao-no-taenai-shokuba-desu.json"]);

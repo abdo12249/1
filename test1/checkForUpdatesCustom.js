82c85e3c26468ec1fc7608024f0f3d3b65e2eb82
abdo12249 <abdobwd87@gmail.com>
2025-12-05 07:42:49 +0200

    تحديث checkForUpdatesCustom.js بإضافة sozai-saishuka-no-isekai-ryokouki.json

--- test1/checkForUpdatesCustom.js
+++ test1/checkForUpdatesCustom.js
@@ -1,1 +1,1 @@
-checkForUpdatesCustom(["https://myanima.ddns.net/test1/episodes/yano-kun-no-futsuu-no-hibi.json"]);
+checkForUpdatesCustom(["https://myanima.ddns.net/test1/episodes/yano-kun-no-futsuu-no-hibi.json", "https://myanima.ddns.net/test1/episodes/sozai-saishuka-no-isekai-ryokouki.json"]);
